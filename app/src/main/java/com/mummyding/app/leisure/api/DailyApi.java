50da068eeccc87a4e29a523bfd8b8254f482e5e9
MummyDing <MummyDingz@gmail.com>
2016-01-01 15:35:16 +0800

    Version 2.0 first commit

--- app/src/main/java/com/mummyding/app/leisure/api/DailyApi.java
+++ app/src/main/java/com/mummyding/app/leisure/api/DailyApi.java
@@ -1,20 +1,21 @@
 /*
- *  Copyright (C) 2015 MummyDing
+ *  *  Copyright (C) 2015 MummyDing
+ *  *
+ *  *  This file is part of Leisure( <https://github.com/MummyDing/Leisure> )
+ *  *
+ *  *  Leisure is free software: you can redistribute it and/or modify
+ *  *  it under the terms of the GNU General Public License as published by
+ *  *  the Free Software Foundation, either version 3 of the License, or
+ *  *  (at your option) any later version.
+ *  *                             ｀
+ *  *  Leisure is distributed in the hope that it will be useful,
+ *  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *  GNU General Public License for more details.
+ *  *
+ *  *  You should have received a copy of the GNU General Public License
+ *  *  along with Leisure.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  This file is part of Leisure( <https://github.com/MummyDing/Leisure> )
- *
- *  Leisure is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *                             ｀
- *  Leisure is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Leisure.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.mummyding.app.leisure.api;
@@ -23,8 +24,13 @@ package com.mummyding.app.leisure.api;
  *  Created by mummyding on 15-11-22.<br>
  *  ZhiHu(link www.zhihu.com) Daily Api<br>
  *  @author MummyDing
- *  @version Leisure 1.0
+ *  @version Leisure 1.0 1.1 / 2.0
  */
 public class DailyApi {
-    public static String daily_url = "https://diy-devz.rhcloud.com/zhihu";
+    // Version 1.0 1.1
+    //public static String daily_url = "https://diy-devz.rhcloud.com/zhihu";
+
+
+    public static String daily_url = "http://news-at.zhihu.com/api/4/news/latest";
+    public static String daily_details_url="http://news-at.zhihu.com/api/4/news/";
 }
