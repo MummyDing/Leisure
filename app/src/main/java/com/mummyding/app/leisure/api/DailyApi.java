155f40d10c89683682d1eb61e5292393a2f0248d
MummyDing <MummyDingz@gmail.com>
2016-01-03 10:07:00 +0800

    add version info

--- app/src/main/java/com/mummyding/app/leisure/api/DailyApi.java
+++ app/src/main/java/com/mummyding/app/leisure/api/DailyApi.java
@@ -31,6 +31,7 @@ public class DailyApi {
     //public static String daily_url = "https://diy-devz.rhcloud.com/zhihu";
 
 
+    // Version 2.0
     public static String daily_url = "http://news-at.zhihu.com/api/4/news/latest";
     public static String daily_details_url="http://news-at.zhihu.com/api/4/news/";
     public static String daily_old_url= "http://news.at.zhihu.com/api/4/news/before/";
